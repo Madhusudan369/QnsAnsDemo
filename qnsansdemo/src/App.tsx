8102365ee59d12c0520355ac2c2e096c42bd6b35
madhu_369 <babloo.ray420@gmail.com>
2025-05-12 12:42:23 +0530

    Added Matching questions with answers as drag n drop

---
+++ qnsansdemo/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import MatchingQnsAns from './Component/MatchingQnsAns'
+
+function App() {
+
+  return (
+    
+    <div className="App">
+      <MatchingQnsAns />
+      </div>
+  )
+}
+
+export default App
